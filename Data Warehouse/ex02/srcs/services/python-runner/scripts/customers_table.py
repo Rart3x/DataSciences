0cf422843ca2e040e49321252dd984f5366f4178
Rart3x <ramjattankeny971@gmail.com>
2023-12-22 21:33:21 +0100

    ./DataWarehouse/ex02'scripts WORK, on Docker NO, need to fix

---
+++ Data Warehouse/ex02/srcs/services/python-runner/scripts/customers_table.py
@@ -0,0 +1,34 @@
+import os
+import psycopg2
+from dateutil import parser
+from colorama import Fore, Style
+
+# Database connection
+conn = psycopg2.connect(
+    dbname="piscineds",
+    host="db",
+    user="kramjatt",
+    password="mysecretpassword",
+    port="5432",
+)
+cursor = conn.cursor()
+
+# Get all tables
+cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+tables = cursor.fetchall()
+
+filtered_tables = [table[0] for table in tables if table[0].startswith('data_202')]
+
+# Merge tables
+if filtered_tables:
+    merge_query = "CREATE TABLE customers AS SELECT * FROM {};".format(' UNION ALL SELECT * FROM '.join(filtered_tables))
+    id_query = "ALTER TABLE customers ADD COLUMN id SERIAL PRIMARY KEY;"
+    cursor.execute(merge_query)
+    cursor.execute(id_query)
+    conn.commit()
+else:
+    print("error: no tables to merge")
+
+# Close connections
+cursor.close()
+conn.close()
---
+++ Data Warehouse/ex02/srcs/services/python-runner/scripts/scripts.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python ./scripts/automatic_table.py
+python ./scripts/customers_table.py
+python ./scripts/delete_dup_row.py
