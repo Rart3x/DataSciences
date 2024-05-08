88a88c186bffcbcdb77ebb76869fb0a684859ae5
Rart3x <ramjattankeny971@gmail.com>
2024-05-08 17:46:49 +0200

    Execute and commit function, handle differents types of booleans [TRUE, true, FALSE, false]

--- Data Engineer/ex02/srcs/services/python-runner/scripts/table.py
+++ Data Engineer/ex02/srcs/services/python-runner/scripts/table.py
@@ -18,13 +18,18 @@ DIRECTORY = '/work/scripts/customers/'
 
 # Errors
 DATE_ERROR = f"{Fore.RED}error: first field must be a date for : {Style.RESET_ALL}"
-FIELD_ERROR = f"{Fore.RED}error: fields must be the same for : {Style.RESET_ALL}"
+FIELD_ERROR = f"{Fore.RED}error: fields types must be the same for : {Style.RESET_ALL}"
 FIELD_NUMBER_ERROR = f"{Fore.RED}error: number of fields must be the same for : {Style.RESET_ALL}"
+
+# Execute and commit
+def executeAndCommit(query):
+    cursor.execute(query)
+    conn.commit()
 
 # Type define
 def defineType(field):
     try:
-        if field.lower() in ["true", "false"]:
+        if field in ["TRUE", "FALSE", "true", "false"]:
             return "BOOLEAN"
         
         int(field)
@@ -41,9 +46,8 @@ def defineType(field):
 # Tables creations
 def createTable(tableName, fields):
     field_definitions = ', '.join([f'"{field}" {fields[field]}' for field in fields])
-    createQuery = f"CREATE TABLE IF NOT EXISTS {tableName} ({field_definitions})"
-    cursor.execute(createQuery)
-    conn.commit()
+    create_query = f"CREATE TABLE IF NOT EXISTS {tableName} ({field_definitions})"
+    executeAndCommit(create_query)
 
 # Tables inserts
 def insertData(tableName, fields, fileContent):
@@ -53,10 +57,9 @@ def insertData(tableName, fields, fileContent):
             print(FIELD_ERROR + tableName + " at line: " + line)
         else:
             values = line.split(',')
-            strippedValues = [f"'{value.strip()}'" for value in values]
-            insert_query = f'INSERT INTO {tableName} ({", ".join(fields)}) VALUES ({", ".join(strippedValues)})'
-            cursor.execute(insert_query)
-            conn.commit()
+            stripped_values = [f"'{value.strip()}'" for value in values]
+            insert_query = f'INSERT INTO {tableName} ({", ".join(fields)}) VALUES ({", ".join(stripped_values)})'
+            executeAndCommit(insert_query)
 
 # CSV treatments
 for filename in os.listdir(DIRECTORY):
