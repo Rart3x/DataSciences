59b5d2648bcba16de573b049a5baf586ecdb9c5a
Rart3x <ramjattankeny971@gmail.com>
2024-05-10 04:40:37 +0200

    Deleted ex02, better scripts.py

--- Data Warehouse/ex01/srcs/services/python-runner/scripts/automatic_table.py
+++ Data Warehouse/ex01/srcs/services/python-runner/scripts/automatic_table.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
-from dateutil import parser
-import importlib.util
+
+from datetime import datetime
 from colorama import Fore, Style
 
 # Database connection
@@ -19,32 +19,48 @@ DIRECTORY = '/work/scripts/customers/'
 
 # Errors
 DATE_ERROR = f"{Fore.RED}error: first field must be a date for : {Style.RESET_ALL}"
-FIELD_ERROR = f"{Fore.RED}error: fields must be the same for : {Style.RESET_ALL}"
+FIELD_ERROR = f"{Fore.RED}error: fields types must be the same for : {Style.RESET_ALL}"
 FIELD_NUMBER_ERROR = f"{Fore.RED}error: number of fields must be the same for : {Style.RESET_ALL}"
+
+# Execute and commit
+def executeAndCommit(query):
+    cursor.execute(query)
+    conn.commit()
 
 # Type define
 def defineType(field):
     try:
-        if field.lower() in ["true", "false"]:
+        if field in ["TRUE", "FALSE", "true", "false"]:
             return "BOOLEAN"
         
         int(field)
-        return "INTEGER"
+        return "BIGINT"
 
     except ValueError:
         try:
-            parser.parse(field)
-            return "DATE"
-
+            float(field)
+            return "FLOAT"
+        
         except ValueError:
-            return "VARCHAR(255)"
+            if len(field) == 1:
+                return "CHAR"
+            try:
+                datetime.strptime(field, '%Y-%m-%d %H:%M:%S %Z')
+                return "DATE"
+
+            except ValueError:
+                try:
+                    datetime.strptime(field, '%m/%d/%Y')
+                    return "DATE"
+                
+                except ValueError:
+                    return "VARCHAR(255)"
 
 # Tables creations
 def createTable(tableName, fields):
     field_definitions = ', '.join([f'"{field}" {fields[field]}' for field in fields])
-    createQuery = f"CREATE TABLE IF NOT EXISTS {tableName} ({field_definitions})"
-    cursor.execute(createQuery)
-    conn.commit()
+    create_query = f"CREATE TABLE IF NOT EXISTS {tableName} ({field_definitions})"
+    executeAndCommit(create_query)
 
 # Tables inserts
 def insertData(tableName, fields, fileContent):
@@ -52,12 +68,11 @@ def insertData(tableName, fields, fileContent):
         lineTypes = [defineType(value.strip()) for value in line.split(',')]
         if lineTypes != [fields[key] for key in fields]:
             print(FIELD_ERROR + tableName + " at line: " + line)
-            return
-        values = line.split(',')
-        strippedValues = [f"'{value.strip()}'" for value in values]
-        insert_query = f'INSERT INTO {tableName} ({", ".join(fields)}) VALUES ({", ".join(strippedValues)})'
-        cursor.execute(insert_query)
-        conn.commit()
+        else:
+            values = line.split(',')
+            stripped_values = [f"'{value.strip()}'" for value in values]
+            insert_query = f'INSERT INTO {tableName} ({", ".join(fields)}) VALUES ({", ".join(stripped_values)})'
+            executeAndCommit(insert_query)
 
 # CSV treatments
 for filename in os.listdir(DIRECTORY):
@@ -67,11 +82,8 @@ for filename in os.listdir(DIRECTORY):
             fileContent = file.readlines()
             if len(fileContent) > 1:
                 fields = {field.strip(): defineType(value.strip()) for field, value in zip(fileContent[0].split(','), fileContent[1].split(','))}
-                if fields[fileContent[0].split(',')[0].strip()] == "DATE":
-                    createTable(tableName, fields)
-                    insertData(tableName, fields, fileContent)
-                else:
-                    print(DATE_ERROR + tableName)
+                createTable(tableName, fields)
+                insertData(tableName, fields, fileContent)
 
 # Close connections
 cursor.close()
--- Data Warehouse/ex02/srcs/services/python-runner/scripts/scripts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python ./scripts/automatic_table.py
-python ./scripts/customers_table.py
-python ./scripts/delete_dup_row.py
