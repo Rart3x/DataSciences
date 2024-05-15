08d11336b55e302184b2b8a7b8dbb5a651800134
Rart3x <ramjattankeny971@gmail.com>
2024-05-15 08:38:19 +0200

    ex02 - #Cleanest way to delete duplicates row in SQL

--- Data Warehouse/ex02/srcs/services/python-runner/scripts/remove_duplicates.py
+++ Data Warehouse/ex02/srcs/services/python-runner/scripts/remove_duplicates.py
@@ -15,7 +15,28 @@ cursor = conn.cursor()
 
 def define_occurencies_q(columns):
     column_definitions = ', '.join([f'{column[0]}' for column in columns])
-    return f"SELECT {column_definitions} FROM customers GROUP BY {column_definitions} HAVING COUNT(*) > 1;"
+    return f"""
+        SELECT {column_definitions}
+        FROM customers 
+        GROUP BY {column_definitions}
+        HAVING COUNT(*) > 1;
+    """
+
+def del_occurencies_q(columns, occurencies):
+    column_definitions = ', '.join([f'{column[0]}' for column in columns])
+    return f"""
+        WITH CTE AS (
+            SELECT ctid, {column_definitions},
+                ROW_NUMBER() OVER(PARTITION BY {column_definitions}) AS RN
+            FROM customers
+        )
+        DELETE FROM customers
+        WHERE ctid IN (
+            SELECT ctid
+            FROM CTE
+            WHERE RN > 1
+        );
+    """
 
 # Querys
 columns_query = "SELECT column_name FROM information_schema.columns WHERE table_name = 'customers';"
@@ -24,7 +45,13 @@ columns_query = "SELECT column_name FROM information_schema.columns WHERE table_
 cursor.execute(columns_query)
 columns = cursor.fetchall()
 
-print(define_occurencies_q(columns))
+# Fetch all duplicates occurencies from 'customers' table
+cursor.execute(define_occurencies_q(columns))
+occurencies = cursor.fetchall()
+
+# Delete all duplicates occurencies from 'customers' table
+print(del_occurencies_q(columns, occurencies))
+cursor.execute(del_occurencies_q(columns, occurencies))
 
 # Close connections
 cursor.close()
